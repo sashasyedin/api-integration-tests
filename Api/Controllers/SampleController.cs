ef436baace651c1195f6940a3eecc0a1a15ed63b
Alexander Ivanov <sashasyedin@outlook.com>
2021-02-09 17:36:12 +0300

    Add SUT project

---
+++ Api/Controllers/SampleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SampleController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public SampleController(ILogger<SampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetRandom(int coeff)
+        {
+            var random = new Random().Next(0, 10);
+            return Ok(new Tuple<int, int>(random, random * coeff));
+        }
+    }
+}
